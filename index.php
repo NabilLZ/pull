3589cface45da9b665d8dea758fc4f744378e918
NabilLZ <77263133+NabilLZ@users.noreply.github.com>
2025-05-16 22:17:54 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- <!DOCTYPE html>
+<html>
+    <head>
+        <title><?php echo "Praktikum Modul 01" ?></title>
+    </head>
+    <body>
+        <?php
+            echo "<p>Hello world</p>";
+            
+        ?>
+    </body>
+</html> -->
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo "Praktikum Modul 01"; ?></title>
+</head>
+<body>
+    <?php
+        $nama = "Muhammad Nabil Fatahilah";
+        $nim = "235150401111063";
+        $prodi = "Sistem Informasi";
+        $universitas = "Universitas Brawijaya";
+
+        echo "<h1>Data Mahasiswa</h1>";
+        echo "<p>Nama: $nama</p>";
+        echo "<p>NIM: $nim</p>";
+        echo "<p>Program Studi: $prodi</p>";
+        echo "<p>Universitas: $universitas</p>";
+    ?>
+</body>
+</html>
+
